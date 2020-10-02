5a8add8f66faaefe4c502fe1ee8e4dee8231cf2e
Miroslav Havezov <mhavezov@gmail.com>
2020-10-02 16:09:40 +0300

    fixed a console warrning

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -11,9 +11,7 @@ const Product = ({ product }) => {
       </Link>
       <Card.Body>
         <Link to={`/products/${product._id}`}>
-          <Card.Title as='div'>
-            <strong>{product.name}</strong>
-          </Card.Title>
+          <Card.Title>{product.name}</Card.Title>
         </Link>
         <Card.Text as='div'>
           <Rating
