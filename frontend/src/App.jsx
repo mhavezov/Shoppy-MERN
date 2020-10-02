24f5ab82d65f41564d485ee2cbf0ea1c34e54656
Miroslav Havezov <mhavezov@gmail.com>
2020-10-02 13:23:08 +0300

    added ProductsPage.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
+import ProductsPage from './pages/ProductsPage';
 
 function App() {
   return (
-    <div className='App'>
-      <Header />
-      <main className='py-3'>
-        <Container>
-          <HomePage />
-        </Container>
-      </main>
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <>
+        <Header />
+        <main className='py-3'>
+          <Container>
+            <Route exact path='/' component={HomePage} />
+            <Route path='/products/:id' component={ProductsPage} />
+          </Container>
+        </main>
+        <Footer />
+      </>
+    </BrowserRouter>
   );
 }
 
